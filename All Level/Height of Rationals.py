cf9ecec13c35be346e3313a46de908aa510a3545
samarth32 <98467014+samarth32@users.noreply.github.com>
2022-08-23 15:48:42 +0530

    Create Height of Rationals.py

---
+++ All Level/Height of Rationals.py
@@ -0,0 +1,20 @@
+Sample 1:
+Input
+Output
+3 19
+19
+Explanation:
+The maximum of \{3, 19\}{3,19} is 1919. Hence the Height of \frac{3}{19} 
+19
+3
+​
+  is 1919.
+
+Sample 2:
+Input
+Output
+27 4
+27
+
+Sol:n=map(int,input().split())
+print(max(n))
